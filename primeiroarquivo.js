9196358e390df047db4498b2cd25e275b45b9619
alana-monique <163845573+alana-monique@users.noreply.github.com>
2024-08-16 11:35:15 -0300

    Add files via upload

--- primeiroarquivo.js
+++ primeiroarquivo.js
@@ -1,3 +1,13 @@
-console.log("Meu primeiro programa: trabalhado com rariaveis ")
-const idade = 16;
-console.log(idade)
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+const idade = 26;
+
+const idadeSoma = idade + 2;
+
+console.log("A soma da idade + 2 é ", idadeSoma); 
+
+console.log("Sua idade é ", idade);
+console.log("Sua idade + 2 é ", idade + 2);
+console.log("Sua idade - 2 é ", idade - 2);
+console.log("Sua idade / 2 é ", idade / 2);
+
